58eb5b16ecd284a3936ee3eda30f9e50a963605a
bivashy <botyrbojey@gmail.com>
2024-04-24 03:08:17 +0500

    Add Post Model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class Post(Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    author_id = models.CharField(max_length=128)
+    image_preview = models.ImageField(upload_to='posts/images', null=False, blank=False)
