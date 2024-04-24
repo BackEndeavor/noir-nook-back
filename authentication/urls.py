e545b0718c82062b2ad10ecc773347041b1f9367
bivashy <botyrbojey@gmail.com>
2024-04-24 20:19:39 +0500

    Return JWT token expiration time

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,12 @@
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.views import UserDetailsView
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenVerifyView
 
 from authentication.views import GoogleConnect, GitHubConnect
 
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
