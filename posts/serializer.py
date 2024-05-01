bbff26338d98d02a60f0f6ddc8637005f83972bd
bivashy <botyrbojey@gmail.com>
2024-05-02 04:06:39 +0500

    Update PostSerializer, add PostListSerializer

--- posts/serializer.py
+++ posts/serializer.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
+
+from authentication.serializer import UserProfileSerializer
+from . import summarizer
 from .models import Post
+
+CONTENT_LIMIT_LENGTH = 256
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = UserProfileSerializer()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'author', 'image_preview', 'content', 'publication_date']
+
+
+class PostListSerializer(PostSerializer):
+    content = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'author', 'image_preview', 'content', 'publication_date']
+
+    def get_content(self, obj):
+        content = obj.content
+        return content[:CONTENT_LIMIT_LENGTH].strip() + (content[CONTENT_LIMIT_LENGTH:] and '...')
+
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'author_id', 'image_preview']
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse
-from rest_framework import viewsets
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, mixins
+from rest_framework.generics import GenericAPIView
 
+from NoirNook.pagination import StandardResultsPagination
 from .models import Post
-from .serializer import PostSerializer
+from .serializer import PostSerializer, PostListSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    pagination_class = StandardResultsPagination
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        return PostSerializer
 
 
 @api_view(['GET'])
