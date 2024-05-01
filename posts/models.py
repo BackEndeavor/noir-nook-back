8683ce9544613312bd1222defb9de8ba8c14d081
bivashy <botyrbojey@gmail.com>
2024-05-02 04:04:47 +0500

    Update models author, and add publication date

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Model
 
+from NoirNook import settings
+
 
 class Post(Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=256)
     content = models.TextField()
-    author_id = models.CharField(max_length=128)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image_preview = models.ImageField(upload_to='posts/images', null=True, blank=True)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
